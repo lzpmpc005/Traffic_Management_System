b17fed8c691871cc6898b7b53aefca6caddc7e70
vincentyelpen <147992850+vincentyelpen@users.noreply.github.com>
2024-01-27 18:32:07 +0100

    Add files via upload (#3)
    Co-authored-by: Hongtao Li <lihongtaoix7@gmail.com>

--- traffic_management_system/traffic_management/urls.py
+++ traffic_management_system/traffic_management/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import (register_owner, register_junction)
+
+from .views import (register_owner, register_junction,register_vehicle)
 
 urlpatterns = [
     path('register_owner', register_owner, name='register_owner'),
-    path('register_junction', register_junction, name='register_junction')
-
+    path('register_junction', register_junction, name='register_junction'),
+    path('register_vehicle',register_vehicle, name='register_vehicle')
 ]
