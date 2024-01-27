3a73f215eff91632c6e6b4cd2463fb3ed2ae55de
ThomasLix7 <lihongtaoix7@gmail.com>
2024-01-27 13:18:51 +0100

    define models: Vehicle,Owner,Junction; create functions to 	register Owner,Junction; Setup urls path

--- traffic_management_system/traffic_management/models.py
+++ traffic_management_system/traffic_management/models.py
@@ -1,1 +1,25 @@
+from django.db import models
 
+
+class Owner(models.Model):
+    Owner_name = models.CharField(max_length=20)
+    Owner_phone = models.CharField(max_length=15)
+    Owner_address = models.CharField(max_length=100)
+    def __str__(self) -> str:
+        return self.Owner_name
+
+class Vehicle(models.Model):
+    Number = models.CharField(max_length=10)
+    Owner = models.ForeignKey(Owner, on_delete=models.SET_NULL, null=True)
+    Color = models.CharField(max_length=10)
+    Producer = models.CharField(max_length=10)
+    Type = models.CharField(max_length=10)
+    Year = models.IntegerField()
+    def __str__(self) -> str:
+        return self.Number
+
+
+class Junction(models.Model):
+    Address = models.CharField(max_length=100)
+    def __str__(self) -> str:
+        return self.Address
--- traffic_management_system/traffic_management/urls.py
+++ traffic_management_system/traffic_management/urls.py
@@ -1,8 +1,8 @@
-# from django.urls import path
-# from .views import (book_list, bulk_delete_books)
-#
-# urlpatterns = [
-#     path('booklist/', book_list, name='BookList'),
-#     path('create_author', create_author, name='Create_Author')
-#
-# ]
+from django.urls import path
+from .views import (register_owner, register_junction)
+
+urlpatterns = [
+    path('register_owner', register_owner, name='register_owner'),
+    path('register_junction', register_junction, name='register_junction')
+
+]
