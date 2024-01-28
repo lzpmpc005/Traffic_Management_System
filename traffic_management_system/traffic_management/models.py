6482a443fdec471dee61265c87fb30e6f8d8f49c
Hongtao Li <lihongtaoix7@gmail.com>
2024-01-28 13:52:19 +0100

    recognize license (#5)
    * update branch LI (#4)
    
    * define models: Vehicle,Owner,Junction; create functions to (#2)
    
    register Owner,Junction; Setup urls path
    
    * Add files via upload (#3)
    
    Co-authored-by: Hongtao Li <lihongtaoix7@gmail.com>
    
    ---------
    
    Co-authored-by: vincentyelpen <147992850+vincentyelpen@users.noreply.github.com>
    
    * recognize_vehicle_license
    
    ---------
    
    Co-authored-by: vincentyelpen <147992850+vincentyelpen@users.noreply.github.com>

--- traffic_management_system/traffic_management/models.py
+++ traffic_management_system/traffic_management/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Owner(models.Model):
     Owner_name = models.CharField(max_length=20)
     Owner_phone = models.CharField(max_length=15)
+    Owner_email = models.CharField(max_length=30, default='<EMAIL>')
     Owner_address = models.CharField(max_length=100)
     def __str__(self) -> str:
         return self.Owner_name
--- traffic_management_system/traffic_management/urls.py
+++ traffic_management_system/traffic_management/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 
-from .views import (register_owner, register_junction,register_vehicle)
+from .views import (register_owner, register_junction, register_vehicle, recognize_vehicle)
 
 urlpatterns = [
     path('register_owner', register_owner, name='register_owner'),
     path('register_junction', register_junction, name='register_junction'),
-    path('register_vehicle',register_vehicle, name='register_vehicle')
+
+    path('register_vehicle', register_vehicle, name='register_vehicle'),
+    path('recognize_vehicle', recognize_vehicle, name='recognize_vehicle')
 ]
