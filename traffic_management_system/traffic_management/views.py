bcfe59c155531b6a58ed35f62722a4200024ea9b
vincentyelpen <147992850+vincentyelpen@users.noreply.github.com>
2024-02-18 18:10:59 +0100

    Update views.py
    pull from LI

--- traffic_management_system/traffic_management/views.py
+++ traffic_management_system/traffic_management/views.py
@@ -6,8 +6,12 @@ import json
 import re
 import random
 from django.core.mail import EmailMessage
-from .models import Vehicle, Junction, Owner, Plates, Log, Fine, DriverLicense
+from .models import Vehicle, Junction, Owner, Plates, Log, Fine, DriverLicense, StatReport, Street
+import io
 import os
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+import matplotlib.pyplot as plt
+from datetime import datetime
 
 sc_number = r'[^\w\s]|\d'
 @csrf_exempt
@@ -69,6 +73,7 @@ def register_junction(request):
         try:
             data = json.loads(request.body.decode("utf-8"))
             address = data.get("address")
+            Jtype = data.get("type")
 
             if not address or address == "":
                 return JsonResponse({'error': "Address not specified"}, status=400)
@@ -77,7 +82,7 @@ def register_junction(request):
                 return JsonResponse({'error': "Address should be a string!"}, status=400)
 
             try:
-                junction, created = Junction.objects.get_or_create(Address=address)
+                junction, created = Junction.objects.get_or_create(Address=address, JType=Jtype)
                 if not created:
                     return JsonResponse({'error': "Junction is already registered!"}, status=400)
 
@@ -120,7 +125,7 @@ def register_vehicle(request):
                 return JsonResponse({'error': "Vehicle condition unqualified"}, status=400)
 
             # Assign an available plate number
-            available_plate = Plates.objects.filter(Status='available').first()
+            available_plate = Plates.objects.filter(Status='reserved').first()
             if not available_plate:
                 return JsonResponse({'error': "No available plates"}, status=400)
 
@@ -151,9 +156,18 @@ def logging(request):
             speed = data.get('Speed')
             light = data.get('Light')
 
+            if 'S' in plateNumber:
+                print('Emergency Vehicle, call notify function here!')
+
+            junction = Junction.objects.get(Address=address)
             plate = Plates.objects.get(Number=plateNumber)
             vehicle = Vehicle.objects.get(PlateNumber_id=plate.id)
-            log = Log.objects.create(Junction=address, Vehicle_PlateNumber=plateNumber, Vehicle_Speed=speed)
+            log = Log.objects.create(Junction=junction, Vehicle_PlateNumber=plateNumber, Vehicle_Speed=speed)
+
+            # detecting congestion and suggest alternative routes to drivers
+            report = StatReport.objects.filter(Junction_id=log.Junction_id).order_by('-id').first()
+            if report.Vehicle_Quantity == 30:
+                send_congestion_notification_email(address, vehicle.Owner_id)
 
             driver_license = DriverLicense.objects.get(Owner_id=vehicle.Owner_id)
 
@@ -187,8 +201,10 @@ def logging(request):
             else:
                 violation = None
 
-            logging_details = (f"{log.Date} {log.Time.strftime('%H:%M:%S')}, {address}, {plateNumber}, {vehicle.Color} "
-                               f"{vehicle.VType}, {speed}km/h, Violation: {violation}")
+            # logging_details = (f"{log.Date} {log.Time.strftime('%H:%M:%S')}, {address}, id:{junction.id}, {plateNumber}, {vehicle.Color} "
+            #                    f"{vehicle.VType}, {speed}km/h, Violation: {violation}")
+
+            logging_details = f"Junction_id: {junction.id}, Vehicle: {plateNumber}"
 
             return JsonResponse({'Logging': logging_details})
 
@@ -311,18 +327,125 @@ def payFine(request):
             return JsonResponse({'error': str(e)}, status=400)
 
 
-def send_congestion_notification_email(junction_name, drivers_emails):
+def send_congestion_notification_email(junction_name, drivers_id):
+    owner = Owner.objects.get(id=drivers_id)
+    junction = Junction.objects.get(Address=junction_name)
+    alter_junction = Junction.objects.get(id=junction.id + 1)
 
-    message = f"Dear driver, please avoid {junction_name} as it is congested."
+    message = (f"Dear Mr/Mrs. {owner.Owner_name}:\n\nThere is a congestion at {junction_name}."
+               f"\nPlease drive to {alter_junction.Address} instead."
+               f"\n\nRegard,\nLeipzig Traffic Police")
 
     subject = 'Congested Area Notification'
     from_email = 'leipzig_traffic@outlook.com'
-
-    email = EmailMessage(subject, message, from_email, drivers_emails)
+    recipient_list = [owner.Owner_email]
+    email = EmailMessage(subject, message, from_email, recipient_list)
     email.send()
 
-    print("Congestion notification email sent successfully.")
 
-junction_name = "Sample Junction"
-drivers_emails = ["drivrs1, drivrs2", "drivrs3"]
-send_congestion_notification_email(junction_name, drivers_emails)
+def generate_report(data, chart, columnX, columnY, time_period, junction_id, report_path):
+    x_data = [item[1] for item in data]
+    y_data = [item[0] for item in data]
+
+    if chart == 'line':
+        sorted_data = sorted(zip(x_data, y_data))
+        x_data_sorted, y_data_sorted = zip(*sorted_data)
+
+        fig, ax = plt.subplots()
+        plt.subplots_adjust(left=0.1, right=0.9, bottom=0.15, top=0.9)
+
+        ax.plot(x_data_sorted, y_data_sorted, marker='o', linestyle='-')
+
+        ax.set_xticklabels(x_data_sorted)
+
+    elif chart == 'bar':
+        fig, ax = plt.subplots()
+        ax.bar(x_data, y_data)
+        ax.set_xticks(x_data)
+
+    ax.set_xlabel(columnX)
+    ax.set_ylabel(columnY)
+    if junction_id != 0:
+        ax.set_title(f'Traffic Flow and Congestion Report\n{time_period}  Junction {junction_id}')
+    else:
+        ax.set_title(f'Traffic Flow and Congestion Report\n{time_period}')
+
+    buffer = io.BytesIO()
+    canvas = FigureCanvas(fig)
+    canvas.draw()
+    fig.savefig(buffer, format='pdf')
+    with open(report_path, 'wb') as f:
+        f.write(buffer.getvalue())
+    plt.close(fig)
+
+    os.startfile(report_path)
+
+
+@csrf_exempt
+def retrieveReport(request):
+    try:
+        date = request.GET.get('Date')  # yyyy-mm-dd (2024-02-09)
+        start_time = request.GET.get('Time_From')  # hh:mm:ss (18:10:00)
+        end_time = request.GET.get('Time_To')
+        junction_id = request.GET.get('Junction_id')  # 1-12
+
+        current_time = datetime.now()
+
+        if not date or date == '':
+            date = current_time.date()
+        if not start_time or start_time == '':
+            start_time = current_time.replace(minute=(current_time.minute // 10) * 10, second=0, microsecond=0).time()
+        if not end_time or end_time == '':
+            end_time = current_time.strftime('%H:%M:%S')
+
+        time_period = f'{date}   {start_time}-{end_time}'
+        report_path = 'E:\\LU_Leipzig\\ProgramClinic\\Project3\\traffic_report.pdf'
+
+        if not junction_id or junction_id == '':
+            data = (StatReport.objects.filter(Date=date, Time__range=(start_time, end_time))
+                    .values_list('Vehicle_Quantity', 'Junction_id'))
+            columnX = 'Junction_id'
+            columnY = 'Vehicle_Quantity'
+            generate_report(data, "bar", columnX, columnY, time_period, 0, report_path)
+
+        else:
+            data = (StatReport.objects.filter(Date=date, Time__range=(start_time, end_time),
+                                              Junction_id=junction_id).values_list('Vehicle_Quantity', 'Time'))
+
+            data = [(quantity, time.hour * 100 + time.minute) for quantity, time in data]
+            columnX = 'Time_Period'
+            columnY = 'Vehicle_Quantity'
+            generate_report(data, "line", columnX, columnY, time_period, junction_id, report_path)
+
+        return JsonResponse({'Traffic Report Period:': time_period,
+                             'Traffic Report Location': report_path})
+
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=400)
+
+
+@csrf_exempt
+def register_route(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body.decode("utf-8"))
+            start_junction_id = data.get("start")
+            end_junction_id = data.get("end")
+            distance = data.get("distance")
+            name = data.get("name")
+
+            start_junction = Junction.objects.get(id=start_junction_id)
+            end_junction = Junction.objects.get(id=end_junction_id)
+
+            try:
+                street = Street.objects.create(Start_junction=start_junction, End_junction=end_junction,
+                                             Distance=distance, Name=name)
+                return JsonResponse({'status': "Route registered successfully", 'route_id': street.id})
+
+            except IntegrityError as e:
+                return JsonResponse({'error': str(e)}, status=400)
+
+        except json.JSONDecodeError:
+            return JsonResponse({'error': "Invalid JSON format"}, status=400)
+
+    return JsonResponse({'error': "Invalid request method"}, status=405)
